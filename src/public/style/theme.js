20f3ef8d58530f5b5ca293dfc8e04bc9256ade40
dadakhalander234 <49512313+dadakhalander234@users.noreply.github.com>
2019-10-01 20:52:50 +0530

    Add files via upload

---
+++ src/public/style/theme.js
@@ -0,0 +1,24 @@
+import DefaultColor from './themes/default/color'
+import ChristmasColor from './themes/christmas/color'
+
+const currentDate = new Date()
+const year = currentDate.getFullYear()
+
+const christmasDates = {
+  begin: new Date(`12/15/${year}`),
+  end: new Date(`01/15/${year}`)
+}
+
+let Color
+
+if (currentDate > christmasDates.begin || currentDate < christmasDates.end) {
+  require('./themes/christmas/color.css')
+  require('./themes/christmas/style.css')
+  Color = ChristmasColor
+} else {
+  require('./themes/default/color.css')
+  require('./themes/default/style.css')
+  Color = DefaultColor
+}
+
+export default Color
---
+++ src/public/style/themes/default/color.js
@@ -0,0 +1,14 @@
+
+export default {
+  orange: '#f7a253',
+  red: '#f75353',
+  green: '#27AE60',
+  blue: '#5264AE',
+  lightBlue: '#7082C2',
+  grey: '#5f5f5f',
+  darkGrey: '#3C3C3C',
+  black: '#1C1C1C',
+  white: '#FFFFFF',
+  transparentGrey: 'rgba(95, 95, 95, 0.5)',
+  lightGrey: '#BBBBBB'
+}
