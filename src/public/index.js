b1f3906e12c902eaabbc42e396553add4c9a3dea
dadakhalander234 <49512313+dadakhalander234@users.noreply.github.com>
2019-10-01 20:47:07 +0530

    Add files via upload

---
+++ src/public/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'material-components-web/dist/material-components-web.css'
+
+import App from './components/app'
+//import registerServiceWorker from './registerServiceWorker'
+
+import './style/theme'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+//registerServiceWorker()
